143b0008c75cbe29aa5be33bf93ac17703af086a
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-02 17:40:31 +0530

    Create multif7.cpp

---
+++ multif7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n=";
+    cin>>n;
+    if(n%7==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    return 0;
+}
